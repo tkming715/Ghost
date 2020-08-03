fafb2568954e4872331aa571be861f66774b4884
Daniel Lockyer <hi@daniellockyer.com>
2020-08-03 20:11:17 +0100

    Updated signature of Bookshelf model listeners
    - as per https://github.com/bookshelf/bookshelf/wiki/Migrating-from-0.15.1-to-1.0.0#different-arguments-on-after-save-event-listeners-saved-created-and-updated, the signature of saved, created and updated listeners has changed to remove the second argument
    - this commits updates our signatures too

--- core/server/models/base/index.js
+++ core/server/models/base/index.js
@@ -291,7 +291,7 @@ ghostBookshelf.Model = ghostBookshelf.Model.extend({
         }
     },
 
-    onCreated(model, attrs, options) {
+    onCreated(model, options) {
         addAction(model, 'added', options);
     },
 
@@ -352,7 +352,7 @@ ghostBookshelf.Model = ghostBookshelf.Model.extend({
             });
     },
 
-    onUpdated(model, attrs, options) {
+    onUpdated(model, options) {
         addAction(model, 'edited', options);
     },
 
